2707dde5f165cd830c4fb3e2f30bbbab63aa5016
Anıl Can Özgök <71594949+anilozgok@users.noreply.github.com>
2022-08-08 15:09:41 +0300

    Project files added to repo

---
+++ Celsius_To_Fahrenheit_ML/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from numpy import sqrt
+
+train = pd.read_csv("training.csv")
+print(train.head())
+
+test = pd.read_csv("testing.csv")
+print(test.head())
+
+model = LinearRegression()
+
+X_train = train.drop("Fahrenheit", axis=1)
+
+y_train = train.loc[:, "Fahrenheit"]
+
+model.fit(X_train, y_train)
+
+X_test = test.drop("Fahrenheit", axis=1)
+y_test = test.loc[:, "Fahrenheit"]
+
+predictions = model.predict(X_test)
+
+comparison = pd.DataFrame({"Actual Values": y_test, "Predictions": predictions})
+
+print(comparison.head())
